c6078c17a6ef8998ed9b7c73860fa33e631b5636
Jason <99857311+Jayrassic@users.noreply.github.com>
2023-12-21 09:41:47 -0500

    Added functionality to display total weight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
+import { useContext, useEffect, useState } from "react";
+import { TotalContext, ProductContext } from "../contexts/TotalsContext";
+
 function Header() {
-  const calcHandler = () => {};
+  const { products, setProducts } = useContext(TotalContext) as ProductContext;
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if (products !== null) {
+      let allWeight = 0;
+
+      products.forEach((product) => {
+        // console.log(product);
+        allWeight += product.totalWeight;
+      });
+
+      setTotal(allWeight);
+    }
+  }, [products]);
 
   return (
     <header>
       <h1>Pallet Counter</h1>
       <div>
-        <h3>Total Weight: {totalWeight}</h3>
-      </div>
-      <div>
-        <button onClick={calcHandler}>Calculate</button>
+        <h3>Total Weight: {total}</h3>
       </div>
     </header>
   );
