b5dbe8434524da88da53f00cb16bfe8dc2a6b126
Jason <99857311+Jayrassic@users.noreply.github.com>
2023-12-21 09:43:41 -0500

    Added Header to return

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 // import ProductForm from "./components/ProductForm";
+import Header from "./components/Header";
 import FormHolder from "./components/FormHolder";
 
 function App() {
   return (
     <>
+      <Header />
       <FormHolder />
-      {/* <ProductForm /> */}
     </>
   );
 }
