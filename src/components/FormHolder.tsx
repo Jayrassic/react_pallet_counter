daadcd72873e2adf784a0bb3ba618b3e54d0861e
Jason <99857311+Jayrassic@users.noreply.github.com>
2023-12-21 14:44:06 -0500

    Added button and functionality to clear all forms

--- src/components/FormHolder.tsx
+++ src/components/FormHolder.tsx
@@ -4,9 +4,14 @@ import { TotalContext, ProductContext } from "../contexts/TotalsContext";
 
 function FormHolder() {
   const [idCount, setIdCount] = useState<number>(1);
-  const { saveNewProduct, products } = useContext(
+  const { saveNewProduct, products, setProducts } = useContext(
     TotalContext
   ) as ProductContext;
+
+  function clearHandler() {
+    setIdCount(1);
+    setProducts(null);
+  }
 
   function clickHandler() {
     setIdCount(() => idCount + 1);
@@ -18,23 +23,34 @@ function FormHolder() {
     });
   }
   useEffect(() => {
-    saveNewProduct({
-      id: 0,
-      name: null,
-      totalWeight: null,
-      totalCount: null,
-    });
-  }, []);
+    if (!products) {
+      saveNewProduct({
+        id: 0,
+        name: null,
+        totalWeight: null,
+        totalCount: null,
+      });
+    }
+  }, [products]);
 
   if (products) {
     return (
       <>
+        <button onClick={clearHandler}>Clear All</button>
+
         {products.map((product) => {
           return <ProductForm key={product.id} id={product.id}></ProductForm>;
         })}
         <button onClick={clickHandler}>Add Additional Form</button>
       </>
     );
+    // } else {
+    //   saveNewProduct({
+    //     id: 0,
+    //     name: null,
+    //     totalWeight: null,
+    //     totalCount: null,
+    //   });
   }
 }
 
