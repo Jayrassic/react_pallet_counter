547dffe87788a33e51064fe32310fbf34d0ec8f0
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-23 16:20:27 -0400

    Created Header test

---
+++ src/components/__test__/Header.test.tsx
@@ -0,0 +1,46 @@
+import { describe, test, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { TotalContextProvider } from "../../contexts/TotalsContext";
+import "@testing-library/jest-dom";
+import Header from "../Header";
+
+[
+  {
+    id: 0,
+    name: "ACS-10-40",
+    totalWeight: 44,
+    totalCount: 2,
+  },
+];
+
+const MockHeader = () => {
+  return (
+    <TotalContextProvider
+      value={[
+        {
+          id: 0,
+          name: "ACS-10-40",
+          totalWeight: 44,
+          totalCount: 2,
+        },
+      ]}
+    >
+      <Header />
+    </TotalContextProvider>
+  );
+};
+
+describe("Header", () => {
+  window.HTMLElement.prototype.scrollIntoView = vi.fn();
+
+  test("Renders properly", () => {
+    render(<MockHeader />);
+  });
+
+  test("Total 0 on render", () => {
+    render(<MockHeader />);
+
+    const totalElement = screen.getByRole("heading", { level: 3 });
+    expect(totalElement).toBeInTheDocument();
+  });
+});
