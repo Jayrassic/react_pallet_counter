2643bb3a57f82f33fb10e5ef8dc8447958fd1b8b
Jason <99857311+Jayrassic@users.noreply.github.com>
2024-04-24 10:46:47 -0400

    Added additonal test

--- src/components/__test__/Header.test.tsx
+++ src/components/__test__/Header.test.tsx
@@ -1,31 +1,65 @@
 import { describe, test, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { TotalContextProvider } from "../../contexts/TotalsContext";
+import { TotalContext, Product } from "../../contexts/TotalsContext";
 import Header from "../Header";
 
-[
+const emptyData: Product[] = [
+  {
+    id: 0,
+    name: null,
+    totalWeight: null,
+    totalCount: null,
+  },
+];
+
+const fakeDataSingle: Product[] = [
   {
     id: 0,
     name: "ACS-10-40",
-    totalWeight: 44,
+    totalWeight: 4,
     totalCount: 2,
   },
 ];
 
-const MockHeader = () => {
+const fakeDataMultiple: Product[] = [
+  {
+    id: 0,
+    name: "ACS-10-40",
+    totalWeight: 4,
+    totalCount: 2,
+  },
+
+  {
+    id: 1,
+    name: "ACS-12-22",
+    totalWeight: 33,
+    totalCount: 50,
+  },
+
+  {
+    id: 2,
+    name: "ACS-31-15MS",
+    totalWeight: 156,
+    totalCount: 52,
+  },
+];
+
+const MockHeader = ({ data }: { data: Product[] }) => {
+  const mockSetProducts = vi.fn();
+  const mockSaveNewProduct = vi.fn();
+  const mockUpdateProduct = vi.fn();
+
   return (
-    <TotalContextProvider
-      value={[
-        {
-          id: 0,
-          name: "ACS-10-40",
-          totalWeight: 44,
-          totalCount: 2,
-        },
-      ]}
+    <TotalContext.Provider
+      value={{
+        products: data,
+        setProducts: mockSetProducts,
+        saveNewProduct: mockSaveNewProduct,
+        updateProduct: mockUpdateProduct,
+      }}
     >
       <Header />
-    </TotalContextProvider>
+    </TotalContext.Provider>
   );
 };
 
@@ -33,13 +67,27 @@ describe("Header", () => {
   window.HTMLElement.prototype.scrollIntoView = vi.fn();
 
   test("Renders properly", () => {
-    render(<MockHeader />);
+    render(<MockHeader data={emptyData} />);
   });
 
   test("Total 0 on render", () => {
-    render(<MockHeader />);
+    render(<MockHeader data={emptyData} />);
 
     const totalElement = screen.getByRole("heading", { level: 3 });
-    expect(totalElement).toBeInTheDocument();
+    expect(totalElement.textContent).toBe("Total Weight: 0 lbs.");
+  });
+
+  test("Renders weight for single product from context", () => {
+    render(<MockHeader data={fakeDataSingle} />);
+
+    const totalElement = screen.getByRole("heading", { level: 3 });
+    expect(totalElement.textContent).toBe("Total Weight: 4 lbs.");
+  });
+
+  test("Renders weight for multiple product from context", () => {
+    render(<MockHeader data={fakeDataMultiple} />);
+
+    const totalElement = screen.getByRole("heading", { level: 3 });
+    expect(totalElement.textContent).toBe("Total Weight: 193 lbs.");
   });
 });
